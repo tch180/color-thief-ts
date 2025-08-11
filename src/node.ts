dab30468ff5c83801d2eb83e5045e6b80f1236c2
tch180 <thopstadredner@gmail.com>
2025-08-11 17:00:57 -0400

    refactor: update image processing to use Jimp and streamline getImageData method

--- src/node.ts
+++ src/node.ts
@@ -1,7 +1,7 @@
 // color quantization, based on Leptonica
-import getPixels from "get-pixels";
-import Core from "./core";
-import type { ColorArray, PaletteOptions } from "./type";
+import Jimp from 'jimp';
+import Core from './core';
+import type { ColorArray, PaletteOptions } from './type';
 
 /**
  *
@@ -10,83 +10,101 @@ import type { ColorArray, PaletteOptions } from "./type";
  *  - a relative path
  *  - an http url
  *  - a data url
- *  - an [in-memory Buffer](http://nodejs.org/api/buffer.html).
+ *  - a Buffer containing image data
  *
  */
-type ImageType = string | { type: string; buffer: Buffer };
+type ImageType = string | Buffer;
 
 class ColorThief extends Core {
-  private getImageData(img: ImageType) {
-    return new Promise<[ImageData, number]>((resolve, reject) => {
-      if (typeof img === "string") {
-        getPixels(img, function (err: any, data: any) {
-          if (err) {
-            reject(err);
-          } else {
-            resolve([data, data.shape[0] * data.shape[1]]);
-          }
-        });
+  private async getImageData(img: ImageType): Promise<[ImageData, number]> {
+    try {
+      let image;
+
+      if (Buffer.isBuffer(img)) {
+        image = await Jimp.read(img as Buffer);
       } else {
-        getPixels(img.buffer, img.type, function (err: any, data: any) {
-          if (err) {
-            reject(err);
-          } else {
-            resolve([data, data.shape[0] * data.shape[1]]);
-          }
-        });
+        image = await Jimp.read(img as string);
       }
-    });
+
+      const width = image.bitmap.width;
+      const height = image.bitmap.height;
+      const pixelCount = width * height;
+
+      // Create ImageData-like structure compatible with existing code
+      const data = new Uint8ClampedArray(pixelCount * 4);
+
+      let dataIndex = 0;
+      image.scan(0, 0, width, height, (x: number, y: number, idx: number) => {
+        // Jimp uses RGBA format in bitmap.data
+        const r = image.bitmap.data[idx + 0];
+        const g = image.bitmap.data[idx + 1];
+        const b = image.bitmap.data[idx + 2];
+        const a = image.bitmap.data[idx + 3];
+
+        data[dataIndex++] = r;
+        data[dataIndex++] = g;
+        data[dataIndex++] = b;
+        data[dataIndex++] = a;
+      });
+
+      // Create ImageData-compatible object
+      const imageData = {
+        data,
+        width,
+        height,
+        colorSpace: 'srgb' as PredefinedColorSpace,
+      } as ImageData;
+
+      return [imageData, pixelCount];
+    } catch (error) {
+      throw new Error(`Failed to process image: ${error}`);
+    }
   }
 
-  public getPalette(
+  public async getPalette(
     img: ImageType,
     colorCount: number,
-    opt?: PaletteOptions<"array">
+    opts?: PaletteOptions<'array'>
   ): Promise<ColorArray[]>;
 
-  public getPalette(
+  public async getPalette(
     img: ImageType,
     colorCount: number,
-    opt?: PaletteOptions<"hex">
+    opts?: PaletteOptions<'hex'>
   ): Promise<string[]>;
 
-  public getPalette(img: ImageType, colorCount = 10, opts?: PaletteOptions) {
-    return new Promise((resolve, reject) => {
-      this.getImageData(img)
-        .then(([imageData, pixelCount]) => {
-          const palette = this._getPalette(
-            imageData,
-            pixelCount,
-            colorCount,
-            opts
-          );
-
-          resolve(palette);
-        })
-        .catch((err) => {
-          reject(err);
-        });
-    });
+  public async getPalette(
+    img: ImageType,
+    colorCount = 10,
+    opts?: PaletteOptions
+  ) {
+    try {
+      const [imageData, pixelCount] = await this.getImageData(img);
+      const palette = this._getPalette(imageData, pixelCount, colorCount, opts);
+      return palette;
+    } catch (error) {
+      throw error;
+    }
   }
 
-  public getColor(
+  public async getColor(
     img: ImageType,
-    opt?: PaletteOptions<"array">
+    opts?: PaletteOptions<'array'>
   ): Promise<ColorArray>;
 
-  public getColor(img: ImageType, opt?: PaletteOptions<"hex">): Promise<string>;
+  public async getColor(
+    img: ImageType,
+    opts?: PaletteOptions<'hex'>
+  ): Promise<string>;
 
-  public getColor(img: ImageType, opts?: PaletteOptions) {
-    return new Promise((resolve, reject) => {
-      this.getImageData(img)
-        .then(([imageData, pixelCount]) => {
-          const color = this._getColor(imageData, pixelCount, opts);
-          resolve(color as any);
-        })
-        .catch((err) => {
-          reject(err);
-        });
-    });
+  public async getColor(img: ImageType, opts?: PaletteOptions) {
+    try {
+      const [imageData, pixelCount] = await this.getImageData(img);
+      const color = this._getColor(imageData, pixelCount, opts);
+      return color;
+    } catch (error) {
+      throw error;
+    }
   }
 }
 
